ef5839e8db657790d11e462ae78bd047dc1a19f6
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-05-30 01:53:51 -0400

    Setup Index view

--- PersonalBlog/Controllers/HomeController.cs
+++ PersonalBlog/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Data;
 using PersonalBlog.Models;
 
 namespace PersonalBlog.Controllers;
@@ -7,15 +9,17 @@ namespace PersonalBlog.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly PersonalBlogContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, PersonalBlogContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        return View(await _context.Articles.ToListAsync());
     }
 
     public IActionResult Privacy()
