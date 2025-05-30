96fd51bd674f57247dcb063546f07f720a995157
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-05-29 22:19:24 -0400

    Create Article model

---
+++ PersonalBlog/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalBlog.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(150)]
+    public string Title { get; set; } = string.Empty;
+    
+    [Required]
+    public string Content { get; set; } = string.Empty;
+    
+    [DisplayName("Publish Date")]
+    [DataType(DataType.Date)]
+    public DateTime PublishDate { get; set; }
+}
