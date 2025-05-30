e25902aad12a6b75c3846deede8545b7fc75fb65
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-05-29 22:37:04 -0400

    Add PersonalBlogContext.cs

---
+++ PersonalBlog/Data/PersonalBlogContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Models;
+
+namespace PersonalBlog.Data;
+
+public class PersonalBlogContext : DbContext
+{
+    public DbSet<Article> Articles { get; set; }
+    
+    public PersonalBlogContext(DbContextOptions options) : base(options)
+    {
+    }
+}
