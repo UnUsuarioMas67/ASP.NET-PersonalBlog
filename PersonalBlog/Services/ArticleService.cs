38a2d04d69afcc9bd8ce49e94b269880e3e80def
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-05-30 02:47:16 -0400

    Add IArticleService.cs and ArticleService.cs

---
+++ PersonalBlog/Services/ArticleService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Data;
+using PersonalBlog.Models;
+
+namespace PersonalBlog.Services;
+
+public class ArticleService : IArticleService
+{
+    private readonly PersonalBlogContext _context;
+
+    public ArticleService(PersonalBlogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Article>> GetAllAsync()
+    {
+        return await _context.Articles.ToListAsync();
+    }
+
+    public async Task<Article?> GetByIdAsync(int id)
+    {
+        return await _context.Articles.FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task CreateAsync(Article article)
+    {
+        _context.Articles.Add(article);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Article article)
+    {
+        _context.Articles.Update(article);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var article = await GetByIdAsync(id);
+        if (article != null)
+        {
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ PersonalBlog/Services/IArticleService.cs
@@ -0,0 +1,12 @@
+﻿using PersonalBlog.Models;
+
+namespace PersonalBlog.Services;
+
+public interface IArticleService
+{
+    Task<List<Article>> GetAllAsync();
+    Task<Article?> GetByIdAsync(int id);
+    Task CreateAsync(Article article);
+    Task UpdateAsync(Article article);
+    Task DeleteAsync(int id);
+}
