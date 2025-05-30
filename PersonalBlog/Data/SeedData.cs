54baf6ee77d8c5f4eec238f93715eb03fdc6b925
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-05-29 23:19:35 -0400

    Create mock articles

---
+++ PersonalBlog/Data/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Models;
+
+namespace PersonalBlog.Data;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context =
+               new PersonalBlogContext(serviceProvider.GetRequiredService<DbContextOptions<PersonalBlogContext>>()))
+        {
+            if (context.Articles.Any())
+                return;
+
+            var articles = LoadArticles();
+            if (articles == null)
+                return;
+
+            context.Articles.AddRange(articles);
+            context.SaveChanges();
+        }
+    }
+
+    private static List<Article>? LoadArticles()
+    {
+        try
+        {
+            var json = File.ReadAllText("mock_articles.json");
+            var articles = JsonSerializer.Deserialize<List<Article>>(json,
+                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            return articles;
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+}
