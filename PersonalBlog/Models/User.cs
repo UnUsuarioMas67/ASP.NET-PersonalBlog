992ae68b05ba6572ecdff8c7d514a117bf594d9e
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-06-13 21:35:28 -0400

    Add User model

---
+++ PersonalBlog/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalBlog.Models;
+
+public class User
+{
+    [Required]
+    [StringLength(50)]
+    public string Username { get; set; } = string.Empty;
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(int.MaxValue)]
+    public string Password { get; set; } = string.Empty;
+}
