6a0b4aa671af2a9e7b1ccf5fdd0a5deb53f5008d
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-06-13 21:35:36 -0400

    Update LoginViewModel.cs

--- PersonalBlog/Models/LoginViewModel.cs
+++ PersonalBlog/Models/LoginViewModel.cs
@@ -4,12 +4,6 @@ namespace PersonalBlog.Models;
 
 public class LoginViewModel
 {
-    [Required]
-    public string Username { get; set; } = string.Empty;
-    
-    [Required]
-    [DataType(DataType.Password)]
-    public string Password { get; set; } = string.Empty;
-
-    public bool Failed { get; set; } = false;
+    public User User { get; set; } = new User();
+    public string? Message { get; set; }
 }
