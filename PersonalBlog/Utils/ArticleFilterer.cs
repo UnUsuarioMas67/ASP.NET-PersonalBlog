a5f9c3075b183cb1364a17bed42e73e7f1a604c0
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-06-14 20:51:05 -0400

    Add ArticleFilterer.cs

---
+++ PersonalBlog/Utils/ArticleFilterer.cs
@@ -0,0 +1,14 @@
+﻿using PersonalBlog.Models;
+
+namespace PersonalBlog.Utils;
+
+public static class ArticleFilterer
+{
+    public static List<Article> FilteredBy(this IEnumerable<Article> articles, string? searchString)
+    {
+        if (string.IsNullOrWhiteSpace(searchString))
+            return articles.ToList();
+        
+        return articles.Where(a => a.Title.Contains(searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+    }
+}
